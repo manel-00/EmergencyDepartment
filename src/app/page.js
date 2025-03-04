dbe545e23516d5024ed9c49a56d9918fa155a418
anasjraidi <101146518+anasjraidi@users.noreply.github.com>
2025-03-04 22:47:07 +0100

    admin

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import Wrapper from './components/wrapper'
+
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
